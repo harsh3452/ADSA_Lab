5469a200a196bc5b65df7ca1acd48ce6e8940f6d
harsh3452 <harshpatil3452@gmail.com>
2026-09-04 18:37:46 +0530

    Fixed

--- Set_2/InsertionSort.c
+++ Set_2/InsertionSort.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 void insertionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int j = i + 1;
-        int key = arr[j];
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
 
-        while (j > 0 && arr[j - 1] > key) {
-            arr[j] = arr[j - 1];
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
             j--;
         }
 
-        arr[j] = key;
+        arr[j + 1] = key;
     }
 }
 
