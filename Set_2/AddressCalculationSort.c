91515610082f64069bfb9d364d276f2c3cb2c93a
harsh3452 <harshpatil3452@gmail.com>
2026-09-07 08:01:38 +0530

    All Sorts Updated

--- Set_2/AddressCalculationSort.c
+++ Set_2/AddressCalculationSort.c
@@ -1,111 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUCKETS 10
+#define SIZE 10
 
-struct Node
-{
+struct Node {
     int data;
     struct Node *next;
 };
 
-void insertSorted(struct Node **head, int value)
-{
-    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+struct Node *head[SIZE];
 
-    newNode->data = value;
-    newNode->next = NULL;
-
-    if (*head == NULL || (*head)->data >= value)
-    {
-        newNode->next = *head;
-        *head = newNode;
-        return;
-    }
-
-    struct Node *current = *head;
-
-    while (current->next != NULL && current->next->data < value)
-    {
-        current = current->next;
-    }
-
-    newNode->next = current->next;
-    current->next = newNode;
+int hash(int x) {
+    return x / 10;   // for values 0-99
 }
 
-void addressCalculationSort(int a[], int n)
-{
-    int min = a[0];
-    int max = a[0];
+void insert(int x) {
+    int a = hash(x);
+    struct Node *p = malloc(sizeof(struct Node));
+    p->data = x;
 
-    for (int i = 1; i < n; i++)
-    {
-        if (a[i] < min)
-        {
-            min = a[i];
-        }
-
-        if (a[i] > max)
-        {
-            max = a[i];
-        }
-    }
-
-    if (min == max)
-    {
+    if (head[a] == NULL || x < head[a]->data) {
+        p->next = head[a];
+        head[a] = p;
         return;
     }
 
-    struct Node *bucket[BUCKETS] = {NULL};
+    struct Node *q = head[a];
+    while (q->next && q->next->data < x)
+        q = q->next;
+
+    p->next = q->next;
+    q->next = p;
+}
+
+void addressSort(int a[], int n) {
+    for (int i = 0; i < SIZE; i++)
+        head[i] = NULL;
 
     for (int i = 0; i < n; i++)
-    {
-        int index = (int)((long long)(a[i] - min) *
-                          (BUCKETS - 1) / (max - min));
-
-        insertSorted(&bucket[index], a[i]);
-    }
+        insert(a[i]);
 
     int k = 0;
+    for (int i = 0; i < SIZE; i++) {
+        struct Node *p = head[i];
 
-    for (int i = 0; i < BUCKETS; i++)
-    {
-        struct Node *current = bucket[i];
-
-        while (current != NULL)
-        {
-            a[k] = current->data;
-            k++;
-
-            struct Node *temp = current;
-            current = current->next;
-            free(temp);
+        while (p) {
+            a[k++] = p->data;
+            struct Node *t = p;
+            p = p->next;
+            free(t);
         }
     }
 }
 
-int main()
-{
-    int a[100];
-    int n;
+int main() {
+    int a[] = {25, 57, 48, 37, 12, 92, 86, 33, 22, 10};
+    int n = sizeof(a) / sizeof(a[0]);
 
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
+    addressSort(a, n);
 
-    printf("Enter elements: ");
     for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &a[i]);
-    }
-
-    addressCalculationSort(a, n);
-
-    printf("Sorted array: ");
-    for (int i = 0; i < n; i++)
-    {
         printf("%d ", a[i]);
-    }
-
-    return 0;
 }
